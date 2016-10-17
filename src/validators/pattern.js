816d1078b810a3e1b982401f6edb174cf0f149de
abrosimov <abrosimov@byndyusoft.com>
2016-10-17 12:30:52 +0500

    add "pattern" validator

---
+++ src/validators/pattern.js
@@ -0,0 +1,3 @@
+import createValidator from '../create-validator';
+
+export default createValidator((value, pattern) => pattern.test(value));
---
+++ src/validators/pattern.spec.js
@@ -0,0 +1,39 @@
+import { expect } from 'chai';
+import pattern from './pattern';
+
+describe.only('validators/pattern', () => {
+    const digits = /^[0-9]+$/;
+    let onlyDigits = pattern('The value must contain only digits')(digits);
+
+    it('return valid result', () => {
+        const result = { isValid: true, error: '' };
+
+        expect(onlyDigits('12345')).to.deep.equal(result);
+        expect(onlyDigits(12345)).to.deep.equal(result);
+        expect(onlyDigits(0)).to.deep.equal(result);
+    });
+
+    it('return invalid result with error message', () => {
+        const result = { isValid: false, error: 'The value must contain only digits' };
+
+        expect(onlyDigits('lorem')).to.deep.equal(result);
+        expect(onlyDigits()).to.deep.equal(result);
+        expect(onlyDigits(null)).to.deep.equal(result);
+        expect(onlyDigits({})).to.deep.equal(result);
+        expect(onlyDigits([])).to.deep.equal(result);
+    });
+
+    it('return invalid result with error message defined as function', () => {
+        onlyDigits = pattern(value => `"${value}" must contain only digits`)(digits);
+
+        expect(onlyDigits('lorem')).to.deep.equal({ isValid: false, error: '"lorem" must contain only digits' });
+        expect(onlyDigits()).to.deep.equal({ isValid: false, error: '"undefined" must contain only digits' });
+        expect(onlyDigits(null)).to.deep.equal({ isValid: false, error: '"null" must contain only digits' });
+        expect(onlyDigits({})).to.deep.equal({ isValid: false, error: '"{}" must contain only digits' });
+        expect(onlyDigits([])).to.deep.equal({ isValid: false, error: '"[]" must contain only digits' });
+        expect(onlyDigits(/\d+/ig)).to.deep.equal({ isValid: false, error: '"/\\d+/gi" must contain only digits' });
+    });
+});
+
+
+//'The value must match provided pattern'
