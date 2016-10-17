eb7a048f9342b23e42b9ac52bc21835632288a79
sadcitizen <sadcitizen@yandex.ru>
2016-10-17 16:42:55 +0500

    add test for "createQueue"

--- src/create-queue.spec.js
+++ src/create-queue.spec.js
@@ -1,33 +1,43 @@
 import { expect } from 'chai';
 import createQueue from './create-queue';
 import createValidator from './create-validator';
+import required from './validators/required';
 
-const isRequired = createValidator(
-    'The value is required',
-    value => value !== undefined && value !== null
-);
-
-const isNumber = createValidator(
-    'The value should be a number',
-    value => typeof value === 'number'
-);
-
-const isEven = createValidator(
-    'The value is not an even number',
-    value => value % 2 === 0
-);
+const isNumber = createValidator(value => typeof value === 'number');
+const isEven = createValidator(value => value % 2 === 0);
 
 describe('createQueue', () => {
-    const queue = createQueue(isRequired, isNumber, isEven);
+    const queue = createQueue(
+        required('The value must be defined')(),
+        isNumber('The value must be a number')(),
+        isEven('The value must be an even number')()
+    );
 
     it('returns a function', () => {
         expect(queue).to.be.a('function');
     });
 
-    it('returns a valid validation result', () => {
+    it('returns a valid result', () => {
         const result = queue(2);
 
-        expect(result).to.have.property('isValid').that.is.a('boolean');
-        expect(result).to.have.property('error').that.is.a('string');
+        expect(result).to.have.property('isValid')
+            .that.is.a('boolean')
+            .that.equal(true);
+
+        expect(result).to.have.property('error')
+            .that.is.a('string')
+            .that.equal('');
+    });
+
+    it('return invalid result with error message', () => {
+        const result = queue(3);
+
+        expect(result).to.have.property('isValid')
+            .that.is.a('boolean')
+            .that.equal(false);
+
+        expect(result).to.have.property('error')
+            .that.is.a('string')
+            .that.equal('The value must be an even number');
     });
 });
