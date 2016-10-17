712d9cb6f6c2f2e8adacabfc8f178cdc786f3a9e
sadcitizen <sadcitizen@yandex.ru>
2016-10-17 16:22:48 +0500

    tests fixes for "createValidator"

--- src/create-validator.spec.js
+++ src/create-validator.spec.js
@@ -2,29 +2,25 @@ import { expect } from 'chai';
 import createValidator from './create-validator';
 
 describe('createValidator', () => {
-    const isEven = createValidator(
-        'The value is not an even number',
-        value => value % 2 === 0
-    );
+    const isEven = createValidator(value => value % 2 === 0);
 
     it('returns a function', () => {
         expect(isEven).to.be.a('function');
+        expect(isEven()).to.be.a('function');
+        expect(isEven()()).to.be.a('function');
     });
 
     it('returns a valid validation result', () => {
-        const result = isEven(2);
+        const result = isEven('The value must be an even number')()(2);
 
         expect(result).to.have.property('isValid').that.is.a('boolean');
         expect(result).to.have.property('error').that.is.a('string');
     });
 
     it('works with error message as function', () => {
-        const isOdd = createValidator(
-            value => `${value} is not an odd number`,
-            value => value % 2 !== 0
-        );
+        const isOdd = createValidator(value => value % 2 !== 0);
 
-        const result = isOdd(2);
+        const result = isOdd(value => `${value} is not an odd number`)()(2);
 
         expect(result).to.have.property('isValid').that.is.a('boolean');
         expect(result).to.have.property('error').that.is.a('string');
