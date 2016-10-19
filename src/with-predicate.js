04b533522fad4d8a16653bd3803a3fc8f28c7d5b
sadcitizen <sadcitizen@yandex.ru>
2016-10-19 16:42:55 +0500

    reformat

--- src/with-predicate.js
+++ src/with-predicate.js
@@ -1,1 +1,3 @@
-export default (fn, predicate) => (...options) => value => predicate(value, ...options) ? fn(...options)(value) : { isValid: true, error: '' };
+export default (fn, predicate) => (...options) => value => predicate(value, ...options)
+    ? fn(...options)(value)
+    : { isValid: true, error: '' };
