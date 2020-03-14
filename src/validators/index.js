df752a6e5f3efb5caa8bf5576c2d759a803d0d2b
sadcitizen <sadcitizen@yandex.ru>
2020-03-14 16:38:44 +0500

    fix build

--- src/validators/index.js
+++ src/validators/index.js
@@ -1,7 +1,9 @@
 import between from './between';
 import equalTo from './equal-to';
 import greaterThan from './greater-than';
+import greaterThanOrEqual from './greater-than-or-equal';
 import lessThan from './less-than';
+import lessThanOrEqual from './less-than-or-equal';
 import oneOf from './one-of';
 import pattern from './pattern';
 import required from './required';
@@ -9,7 +11,9 @@ import required from './required';
 export { between };
 export { equalTo };
 export { greaterThan };
+export { greaterThanOrEqual };
 export { lessThan };
+export { lessThanOrEqual };
 export { oneOf };
 export { pattern };
 export { required };
@@ -18,7 +22,9 @@ export default {
     between,
     equalTo,
     greaterThan,
+    greaterThanOrEqual,
     lessThan,
+    lessThanOrEqual,
     oneOf,
     pattern,
     required
