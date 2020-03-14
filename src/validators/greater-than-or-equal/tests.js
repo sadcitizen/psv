a1dfac8ffe6ae4991ef2c4deccced33d0518e99c
sadcitizen <sadcitizen@yandex.ru>
2020-03-14 10:18:54 +0500

    migrate to jest

--- src/validators/greater-than-or-equal/tests.js
+++ src/validators/greater-than-or-equal/tests.js
@@ -1,21 +1,21 @@
-import { expect } from 'chai';
 import greaterThan from '.';
 
 describe('validators/greaterThanOrEqual', () => {
-    let greaterThanFive = greaterThan('The value must be greater than 5', 5);
+    let greaterThanFive = greaterThan('The value must be greater than or equal 5', 5);
     const bits = [0, 1];
     const hi = 'hi';
 
     test('return valid result', () => {
         const result = { isValid: true, error: '' };
 
+        expect(greaterThanFive(5)).toEqual(result);
         expect(greaterThanFive(6)).toEqual(result);
         expect(greaterThanFive([0, 1, 2, 3, 4, 5, 6, 7, 8, 9].length)).toEqual(result);
         expect(greaterThanFive('lorem ipsum'.length)).toEqual(result);
     });
 
     test('return invalid result with error message', () => {
-        const result = { isValid: false, error: 'The value must be greater than 5' };
+        const result = { isValid: false, error: 'The value must be greater than or equal 5' };
 
         expect(greaterThanFive(4)).toEqual(result);
         expect(greaterThanFive(bits.length)).toEqual(result);
--- src/validators/less-than-or-equal/tests.js
+++ src/validators/less-than-or-equal/tests.js
@@ -1,8 +1,7 @@
-import { expect } from 'chai';
 import lessThan from '.';
 
 describe('validators/lessThanOrEqual', () => {
-    let lessThanFive = lessThan('The value must be less than 5', 5);
+    let lessThanFive = lessThan('The value must be less than or equal 5', 5);
     const digits = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
     const lorem = 'lorem ipsum';
 
@@ -10,12 +9,13 @@ describe('validators/lessThanOrEqual', () => {
         const result = { isValid: true, error: '' };
 
         expect(lessThanFive(4)).toEqual(result);
+        expect(lessThanFive(5)).toEqual(result);
         expect(lessThanFive([0, 1].length)).toEqual(result);
         expect(lessThanFive('hi'.length)).toEqual(result);
     });
 
     test('return invalid result with error message', () => {
-        const result = { isValid: false, error: 'The value must be less than 5' };
+        const result = { isValid: false, error: 'The value must be less than or equal 5' };
 
         expect(lessThanFive(14)).toEqual(result);
         expect(lessThanFive(digits.length)).toEqual(result);
